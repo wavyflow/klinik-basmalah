8d55d40088859ed09c5f651edca11840021b05d4
Ashary Vermaysha <avermaysha@gmail.com>
2023-06-21 08:17:20 +0700

    style: fix capitalization and newline at end of file

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'form_validation', 'session','Dompdf_gen');
+$autoload['libraries'] = array('database', 'form_validation', 'session','dompdf_gen');
 
 /*
 | -------------------------------------------------------------------
